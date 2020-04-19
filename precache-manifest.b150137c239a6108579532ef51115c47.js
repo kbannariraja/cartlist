fb21fb4074432c24e367484a1a22a91c063ae8f1
Bannari raja <emb-bannraj@embitel.com>
2020-04-19 17:01:27 +0530

    Updates

---
+++ precache-manifest.b150137c239a6108579532ef51115c47.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f4208ff406d9d392f78940bdadfb940",
+    "url": "/cartlist/index.html"
+  },
+  {
+    "revision": "120f974e71deca1f97c7",
+    "url": "/cartlist/static/css/2.11977047.chunk.css"
+  },
+  {
+    "revision": "cd6613ea8f8966afd361",
+    "url": "/cartlist/static/css/main.a724b811.chunk.css"
+  },
+  {
+    "revision": "120f974e71deca1f97c7",
+    "url": "/cartlist/static/js/2.5d241bc2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cartlist/static/js/2.5d241bc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd6613ea8f8966afd361",
+    "url": "/cartlist/static/js/main.6071af82.chunk.js"
+  },
+  {
+    "revision": "3be4f3f4cbda87738b05",
+    "url": "/cartlist/static/js/runtime-main.23f8b949.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/cartlist/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/cartlist/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/cartlist/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/cartlist/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/cartlist/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/cartlist/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/cartlist/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/cartlist/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/cartlist/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/cartlist/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
